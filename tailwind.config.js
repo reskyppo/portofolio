89de90649f22489bca102e98cf367bf52cd3a84f
Reskyppo <reskybeen@gmail.com>
2020-10-11 22:13:59 +0700

    still trying

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
   },
-  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  purge: [],
   target: "relaxed",
   prefix: "",
   important: false,
