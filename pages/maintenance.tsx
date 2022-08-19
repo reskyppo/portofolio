7b8932d77ebb3a6d8730542a10335195dd889112
Reskyppo <reskybeen@gmail.com>
2022-08-19 18:49:20 +0700

    feat : init maintenance redirect

---
+++ pages/maintenance.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const maintenance = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-xl font-semibold">
+        This site is currently under maintenance
+      </h1>
+    </div>
+  );
+};
+
+export default maintenance;
